616902fe17a72a0c81fef4aa1f4c48a33ce3a017
dlyubanevich <dlyubanevich@gmail.com>
2021-03-10 19:45:11 +0300

    add authentification

--- Homework #11/src/main/java/ru/otus/dlyubanevich/config/SecurityConfiguration.java
+++ Homework #11/src/main/java/ru/otus/dlyubanevich/config/SecurityConfiguration.java
@@ -2,13 +2,14 @@ package ru.otus.dlyubanevich.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import ru.otus.dlyubanevich.service.LibraryUserDetailService;
 
 @EnableWebSecurity
@@ -16,23 +17,29 @@ import ru.otus.dlyubanevich.service.LibraryUserDetailService;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final LibraryUserDetailService libraryUserDetailService;
+    private final AuthenticationEntryPoint AuthenticationEntryPoint;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-           .csrf().disable()
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/**").authenticated()
+           .cors().and().csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(AuthenticationEntryPoint)
            .and()
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/**").authenticated()
-           .and()
-                .authorizeRequests().antMatchers(HttpMethod.DELETE, "/**").authenticated()
-           .and()
-           .formLogin().loginProcessingUrl("/login").usernameParameter("login").passwordParameter("password");
+                .authorizeRequests()
+                .antMatchers("/api/auth").permitAll()
+           .anyRequest().authenticated();
+
     }
 
     @Override
